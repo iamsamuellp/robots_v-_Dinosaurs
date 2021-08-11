aa46dcfca38146878e7cc33376918c6963e841d5
Samuel McKnight <samuellp@gmail.comgit statusgit config --global user.user.email iamsamuellp@gmail.com>
2021-08-11 18:14:24 -0500

    working on constuctors in battlefield.py

--- battlefield.py
+++ battlefield.py
@@ -13,9 +13,18 @@ class Battlefield:
         self.battlefield_name = name
         self.fleet = Fleet("Red Fleet")
         self.herd = Herd("Dino Horde 1")
-    
-    def 
+        self.game=''     
+        self.welcome =''
+        self.battle = ''
+    def run_game(self,game):
+        self.game = game
+        print("The game begins")
 
+    def display_welcome(self,welcome):
+        self.welcome =welcome
+        
+    def battle_round(self):
+        self.battle
 
     def add_robots_to_fleet(self):
         self.fleet.add_robots()
@@ -24,6 +33,5 @@ class Battlefield:
     def add_dinos_to_herd(self):
         self.herd.add_dinos()
 
-    def battle_round(self):
 
         
